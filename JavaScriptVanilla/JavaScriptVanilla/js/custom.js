961db53ed7731b0c67dd6b88c886ab1fcd507432
Dennis Mirvoda <denmir@yahoo.com>
2017-04-24 12:35:38 -0400

    initial commit

---
+++ JavaScriptVanilla/JavaScriptVanilla/js/custom.js
@@ -0,0 +1,22 @@
+﻿//function swaps images from on to Off - Dennis
+function swapImageToTurnOffOnKeyLock(img) {
+    var onImg = "locked.png";
+
+    var sourceOnImg = "images/locked.png";
+    var sourceOffImg = "images/unlocked.png";
+
+    var currentImageSrc = document.getElementById("lockImage").src;
+
+    //get just the name of the image
+    if (currentImageSrc.indexOf('/') >= 0) {
+        currentImageSrc = currentImageSrc.substring(currentImageSrc.lastIndexOf('/') + 1);
+    }
+
+    if (currentImageSrc == onImg) {
+        img.setAttribute('src', img.getAttribute('src').replace(sourceOnImg, sourceOffImg));
+    } else {
+        img.setAttribute('src', img.getAttribute('src').replace(sourceOffImg, sourceOnImg));
+    }
+
+}
+
